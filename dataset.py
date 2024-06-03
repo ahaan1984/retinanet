1bfa9c86a7d66da61d5dc107da9eb5d09d960415
ahaan <ahaansekhar1984@gmail.com>
2024-06-03 19:52:51 +0530

    refactor dataset.py

--- dataset.py
+++ dataset.py
@@ -1,38 +1,215 @@
+import sys
 import os
 import torch
-import cv2
-import torch.utils.data as data
-from pycocotools import COCO
+import numpy as np
+import random 
 
-class CocoDataset(data.Dataset):
-    def __init__(self, root, annotation, transform=None, target_transform=None):
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from torch.utils.data.sampler import Sampler
+
+from pycocotools.coco import COCO
+import cv2
+
+class CocoDataset(Dataset):
+    def __init__(self, root, set_name='train2017', transform=None):
         self.root = root
-        self.coco = COCO(annotation)
-        self.ids = list(self.coco.imgs.keys())
+        self.set_name = set_name
+        self.coco = COCO(os.path.join(self.root, 'annotations', 'instances_' + self.set_name + '.json'))
+        self.ids = self.coco.getImgIds()
         self.transform = transform
-        self.target_transform = target_transform
+
+    def load_classes(self):
+        categories = self.coco.loadCats(self.coco.getCatIds())
+        categories.sort(key=lambda x: x['id'])
+
+        self.classes = {}
+        self.coco_labels = {}
+        self.coco_labels_inv = {}
+
+        for c in categories:
+            self.coco_labels[len(self.classes)] = c['id']
+            self.coco_labels_inv[c['id']] = len(self.classes)
+            self.classes[c['name']] = len(self.classes)
+
+    def load_image(self, image_idx):
+        image_info = self.coco.loadImgs(self.image_ids[image_idx])[0]
+        path = os.path.join(self.root_dir, 'images', self.set_name, image_info['file_name'])
+        image = cv2.imread(path)
+        if len(image.shape) == 2:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        else:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        return image.astype(np.float32)/255.0
+    
+    def load_annotations(self, image_index):
+        # get ground truth annotations
+        annotations_ids = self.coco.getAnnIds(imgIds=self.image_ids[image_index], iscrowd=False)
+        annotations     = np.zeros((0, 5))
+        if len(annotations_ids) == 0:
+            return annotations
+
+        coco_annotations = self.coco.loadAnns(annotations_ids)
+        for idx, a in enumerate(coco_annotations):
+            if a['bbox'][2] < 1 or a['bbox'][3] < 1:
+                continue
+            annotation        = np.zeros((1, 5))
+            annotation[0, :4] = a['bbox']
+            annotation[0, 4]  = self.coco_label_to_label(a['category_id'])
+            annotations       = np.append(annotations, annotation, axis=0)
+        annotations[:, 2] = annotations[:, 0] + annotations[:, 2]
+        annotations[:, 3] = annotations[:, 1] + annotations[:, 3]
+
+        return annotations
+
 
     def __getitem__(self, idx):
-        coco = self.coco
-        img_id = self.ids[idx]
-        ann_ids = coco.getAnnIds(imgIds=img_id)
-
-        target = coco.loadAnns(ann_ids)
-        target = torch.unsqueeze(torch.Tensor(target[0]['bbox']), -1)
-
-        path = coco.loadImgs(img_id)[0]['file_name']
-        img = cv2.imread(os.path.join(self.root, path))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        if self.transform is not None:
-            img = self.transform(img)
-        
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return img, target
-    
+        image = self.load_image(idx)
+        annot = self.load_annotations(idx)
+        sample = {
+            'image': image,
+            'annotation': annot
+            }
+        if self.transform:
+            sample = self.transform
+        return sample
 
     def __len__(self):
         return len(self.ids)
+    
+    def coco_label_to_label(self, coco_label):
+        return self.coco_labels_inv[coco_label]
 
+    def label_to_coco_label(self, label):
+        return self.coco_labels[label]
+
+    def image_aspect_ratio(self, image_index):
+        image = self.coco.loadImgs(self.image_ids[image_index])[0]
+        return float(image['width']) / float(image['height'])
+
+    def num_classes(self):
+        return 80
+    
+def collater(data):
+    imgs = [s['img'] for s in data]
+    annots = [s['annot'] for s in data]
+    scales = [s['scale'] for s in data]
+        
+    widths = [int(s.shape[0]) for s in imgs]
+    heights = [int(s.shape[1]) for s in imgs]
+    batch_size = len(imgs)
+
+    max_width = np.array(widths).max()
+    max_height = np.array(heights).max()
+
+    padded_imgs = torch.zeros(batch_size, max_width, max_height, 3)
+
+    for i in range(batch_size):
+        img = imgs[i]
+        padded_imgs[i, :int(img.shape[0]), :int(img.shape[1]), :] = img
+
+    max_num_annots = max(annot.shape[0] for annot in annots)
+    if max_num_annots > 0:
+        annot_padded = torch.ones((len(annots), max_num_annots, 5)) * -1
+        if max_num_annots > 0:
+            for idx, annot in enumerate(annots):
+                if annot.shape[0] > 0:
+                    annot_padded[idx, :annot.shape[0], :] = annot
+    else:
+        annot_padded = torch.ones((len(annots), 1, 5)) * -1
+
+
+    padded_imgs = padded_imgs.permute(0, 3, 1, 2)
+    return {'img': padded_imgs, 'annot': annot_padded, 'scale': scales}
+
+class Resizer(object):
+    def __call__(self, sample, min_side=608, max_side=1024):
+        image, annots = sample['img'], sample['annot']
+        rows, cols, cns = image.shape
+
+        smallest_side = min(rows, cols)
+        scale = min_side / smallest_side
+        largest_side = max(rows, cols)
+        if largest_side * scale > max_side:
+            scale = max_side / largest_side
+
+        image = cv2.resize(image, (int(round(cols*scale)), int(round(rows*scale))))
+        rows, cols, cns = image.shape
+        pad_w = 32 - rows%32
+        pad_h = 32 - cols%32
+
+        new_image = np.zeros((rows + pad_w, cols + pad_h, cns)).astype(np.float32)
+        new_image[:rows, :cols, :] = image.astype(np.float32)
+
+        annots[:, :4] *= scale
+        return {'img': torch.from_numpy(new_image), 'annot': torch.from_numpy(annots), 'scale': scale}
+
+class Augmenter(object):
+    def __call__(self, sample, flip_x=0.5):
+
+        if np.random.rand() < flip_x:
+            image, annots = sample['img'], sample['annot']
+            image = image[:, ::-1, :]
+
+            rows, cols, channels = image.shape
+
+            x1 = annots[:, 0].copy()
+            x2 = annots[:, 2].copy()
+            
+            x_tmp = x1.copy()
+
+            annots[:, 0] = cols - x2
+            annots[:, 2] = cols - x_tmp
+            sample = {'img': image, 'annot': annots}
+
+        return sample
+
+class Normalizer(object):
+
+    def __init__(self):
+        self.mean = np.array([[[0.485, 0.456, 0.406]]])
+        self.std = np.array([[[0.229, 0.224, 0.225]]])
+
+    def __call__(self, sample):
+        image, annots = sample['img'], sample['annot']
+        return {'img':((image.astype(np.float32)-self.mean)/self.std), 'annot': annots}
+
+class UnNormalizer(object):
+    def __init__(self, mean=None, std=None):
+        if mean is None:
+            self.mean = [0.485, 0.456, 0.406]
+        else:
+            self.mean = mean
+        if std is None:
+            self.std = [0.229, 0.224, 0.225]
+        else:
+            self.std = std
+
+    def __call__(self, tensor):
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.mul_(s).add_(m)
+        return tensor
+
+class AspectRatioBasedSampler(Sampler):
+    def __init__(self, data_source, batch_size, drop_last):
+        self.data_source = data_source
+        self.batch_size = batch_size
+        self.drop_last = drop_last
+        self.groups = self.group_images()
+
+    def __iter__(self):
+        random.shuffle(self.groups)
+        for group in self.groups:
+            yield group
+
+    def __len__(self):
+        if self.drop_last:
+            return len(self.data_source) // self.batch_size
+        else:
+            return (len(self.data_source) + self.batch_size - 1) // self.batch_size
+
+    def group_images(self):
+        order = list(range(len(self.data_source)))
+        order.sort(key=lambda x: self.data_source.image_aspect_ratio(x))
+        return [[order[x % len(order)] for x in range(i, i + self.batch_size)] for i in range(0, len(order), self.batch_size)]
